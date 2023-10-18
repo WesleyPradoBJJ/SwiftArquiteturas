d917c18aa8eba60a40c0088ad418a9ba89e95e59
Wesley Prado <wesleypradobjj@gmail.com>
2023-10-18 18:52:25 +0400

    Infrastructure for API created

---
+++ SwiftArquiteturas/Infrastructure/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  SwiftArquiteturas
+//
+//  Created by Wesley Prado on 18/10/2023.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct ParametersKeys {
+        static let body = "body"
+        static let userModel = "userModel"
+    }
+}
